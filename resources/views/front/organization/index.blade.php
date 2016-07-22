496413df166786d1ff63c774a70cc9ea9b5e9ce8
growdeuno <grow.deuno@gmail.com>
2016-07-22 21:17:56 +0800

    edit halaman organisasi

--- resources/views/front/organization/index.blade.php
+++ resources/views/front/organization/index.blade.php
@@ -12,13 +12,15 @@
     <br>
     <br>
   <div class="row">
-      <div class="col-md-6">
+      <div class="col-md-12">
+          <h1 class="text-center">Video Profil KPPN Benteng</h1>
         <div class="embed-responsive embed-responsive-4by3">
+
         <iframe src="https://www.youtube.com/embed/{{$organization->embed}}" class="embed-responsive-item" class="center-block"></iframe>
         </div>
       </div>
   </div>
-  <p>
+  <p class="text-danger">
     Perhatian!! Video ini dapat disaksikan jika jaringan internet Anda mengizinkan akses ke <i>Youtube</i>.
   </p>
   @endforeach
